c16ad7d597ac589dc38048db17e8d26d4bc4b57c
ggutierr22 <tuo94731@temple.edu>
2026-04-29 19:11:14 -0400

    Update cypress.config.mjs

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "b880b9b7-91b9-4621-b815-c0bf74223276",
   e2e: {
     baseUrl: 'http://localhost:8080',
     supportFile: false,
