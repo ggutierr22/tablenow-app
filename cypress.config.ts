057f35d36ae328914b4e8ccde0edb80ccbe29df8
ggutierr22 <tuo94731@temple.edu>
2026-04-29 23:00:42 -0400

    Add new E2E tests for tablenow-app

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "knwcct",
   e2e: {
     baseUrl: 'http://localhost:8080',
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
